bccbd955d80ec5a52645182daecc4cecdb494ad1
Guilherme Mattos <guilhermevsmattos@outlook.com>
2020-06-22 11:40:01 -0300

    Creating Migrations and Models to Users and Books

--- src/server.js
+++ src/server.js
@@ -10,4 +10,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 3333);
+app.listen(process.env.PORT || 3333, () => {
+  console.log(`Servidor na porta ${process.env.PORT || '3333'}`);
+});
