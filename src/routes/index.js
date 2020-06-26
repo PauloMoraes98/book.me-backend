35bd7b647cd1e991cefc096da75cd3188bfb8655
Guilherme Mattos <guilhermevsmattos@outlook.com>
2020-06-26 10:21:37 -0300

    Creating and configing middleware to login

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const authMiddleware = require('../middlewares/auth');
 const User = require('../controllers/User')
 
 const routes = express.Router();
 
-routes.get('/user', User.index);
 routes.post('/user', User.store);
+routes.post('/singin', User.singin);
+
+routes.use(authMiddleware);
+
+routes.get('/user', User.index);
 routes.put('/user/:id', User.update);
 routes.delete('/user/:id', User.delete);
 
