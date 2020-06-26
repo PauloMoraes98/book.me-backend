2a4226eaf8631b934c3a3462a1f19b5821cf54a7
Guilherme Mattos <guilhermevsmattos@outlook.com>
2020-06-26 10:13:44 -0300

    Droping test references

--- src/database/index.js
+++ src/database/index.js
@@ -1,13 +1,11 @@
 const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
 
-const Test2 = require('../models/Test2');
 const User = require('../models/User');
 const Book = require('../models/Book');
 
 const connection = new Sequelize(dbConfig);
 
-Test2.init(connection);
 User.init(connection);
 Book.init(connection);
 
