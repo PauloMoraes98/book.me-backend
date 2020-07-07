385497a786ad6d6bb0b77c63e51021d9e4380382
Paulo Cesar <paulo.csm_@hotmail.com>
2020-07-06 23:22:54 -0300

    creating new routes for queries

--- src/routes/index.js
+++ src/routes/index.js
@@ -19,4 +19,6 @@ routes.get('/book', Book.index);
 routes.put('/book/:id', Book.update);
 routes.delete('/book/:id', Book.delete);
 
+routes.get('/book/:id', Book.indexById);
+
 module.exports = routes;
